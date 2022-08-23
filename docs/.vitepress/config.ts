c4dfdb0583772864385c9bb43d23297effe4e9bc
xxy <xxy@xxy5.com>
2022-08-23 15:27:29 +0800

    update: 修改build导出目录位置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   head,
   lastUpdated: true,
   // ignoreDeadLinks: true,
+  outDir: '../dist',
   themeConfig: {
     logo: '/images/logo.png',
     // siteTitle: '全栈小咸鱼',
